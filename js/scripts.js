c17eb4c7af50df60aee7fc0bc6369a5ddec073c0
Sam Kirsch <samdkirsch2@gmail.com>
2017-01-13 09:56:43 -0800

    populate the page with different path options and add a hide styling and javascript functionality to each indivual section

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+  $(".intro").show();
 
+  $(".next-button").click(function() {
+    // this button hides the current page and shows the next one
+    var currentPage = $(this).parent().parent();
+    currentPage.hide();
+    currentPage.next().show();
+  });
+
+});
