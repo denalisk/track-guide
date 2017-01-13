8fe0fd17f2de76aa2d5b008a9d26a544bdfa0cd7
Sam Kirsch <samdkirsch2@gmail.com>
2017-01-13 10:35:42 -0800

    script the info section to show or hide based on mouseover/mouseexit, and rearrange the divs around the info section

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-  $(".intro").show();
+  $(".question-1").show();
 
   $(".next-button").click(function() {
     // this button hides the current page and shows the next one
     var currentPage = $(this).parent().parent();
     currentPage.hide();
     currentPage.next().show();
+  });
+
+  $(".info-button").mouseenter(function() {
+    // this button shows the info on mouseover
+    $(this).next().show();
+    $(this).hide();
+
+  });
+
+  $(".info").mouseleave(function() {
+    // this button hides the info on leaving mouseover
+    $(this).hide();
+    $(this).prev().show();
   });
 
 });
